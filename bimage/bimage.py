cb99c90a710c9b58ccc6f5bb1dec68571d95a092
Chris Calloway <cbc@chriscalloway.org>
2023-06-16 13:12:25 -0400

    Add a CLI parser.

--- bimage/bimage.py
+++ bimage/bimage.py
@@ -1,8 +1,9 @@
 """The bimage module."""
 
-
+import argparse
 import github3
 import git
+
 
 
 def bimage(gitorg, reponame, branch_or_tag):
@@ -12,3 +13,12 @@ def bimage(gitorg, reponame, branch_or_tag):
         repo = gh.repository(gitorg, reponame)
         clone = git.Repo.clone_from(repo.clone_url, reponame)
         clone.git.checkout(branch_or_tag)
+
+
+if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description="Arguments for bimage.")
+        parser.add_argument("gitorg", nargs="?")
+        parser.add_argument("reponame", nargs="?")
+        parser.add_argument("branch_or_tag", nargs="?")
+        args = parser.parse_args()
+        bimage(args.gitorg, args.reponame, args.branch_or_tag)
